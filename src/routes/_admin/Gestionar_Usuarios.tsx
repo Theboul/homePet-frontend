db5093ad183b44082bd45e46f5721f8c6778fa88
Theboul <brad356alex@gmail.com>
2026-03-31 00:23:20 -0400

    -redux.v1

--- src/routes/_admin/Gestionar_Usuarios.tsx
+++ src/routes/_admin/Gestionar_Usuarios.tsx
@@ -2,17 +2,7 @@ import { createFileRoute, redirect } from '@tanstack/react-router'
 import { Gestionar_Usuarios } from '../app/features/AutenticacionySeguridad'
 import { store } from '#/store/store'
 
-<<<<<<< HEAD
-export const Route = createFileRoute('/Gestionar_Usuarios')({
-  beforeLoad: () => {
-    const { isAuthenticated } = store.getState().auth
-    if (!isAuthenticated) {
-      throw redirect({ to: '/login' })
-    }
-  },
-=======
 export const Route = createFileRoute('/_admin/Gestionar_Usuarios')({
->>>>>>> 7fc350d977279f5098a40e560ca8beb7a3be0b3c
   component: App,
 })
 
